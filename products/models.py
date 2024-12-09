2c24117a420787afb0ef3662e559058d0d39c40f
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-12-09 16:08:25 +0200

    Migration Completed

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     stock = models.PositiveIntegerField(default=0)
     category = models.CharField(max_length=255, blank=True)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
