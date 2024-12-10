6ddb486d654ca6a7d4f5b5b767c76bfcc2504cfb
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-12-10 16:30:37 +0200

    Filter Products With Category Name

--- products/views.py
+++ products/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 # from rest_framework import permissions
 from .models import Products
 from .serializers import ProductsSerializer
 
 class ProductsViewSet(viewsets.ModelViewSet):
-    queryset = Products.objects.all()
+    # queryset = Products.objects.all()
     serializer_class = ProductsSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Products.objects.all()
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__iexact=category)
+        return queryset
