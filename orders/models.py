3b8e0f8d2889ef122710866c32395f65cfffac6a
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-12-14 15:46:38 +0200

    added items table to the database

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Orders(models.Model):
 
-    product_id = models.ForeignKey("products.Products", on_delete=models.CASCADE)
+    # product = models.ForeignKey("products.Products", on_delete=models.CASCADE)
+    user = models.ForeignKey("customers.Customer", on_delete=models.CASCADE, blank=True)
     quantity = models.IntegerField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    # total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     order_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default="Pending")
-    user_id = models.ForeignKey("customers.Customer", on_delete=models.CASCADE, blank=True)
+    
 
     def __str__(self):
-        return self.order_id
+        return f"Order {self.id} - Status: {self.status}"
+
+
+class Items(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey("products.Products", on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Item {self.id} - Order: {self.order.id}, Product: {self.product}"
